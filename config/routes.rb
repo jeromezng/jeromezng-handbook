c136230f7dce8a4f1c9193e48fc2d7b5e96a9c1c
jeromezng <>
2022-06-06 18:16:08 -0700

    Set routes root to pages#index, organize routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :pages
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Home
+  root 'pages#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Authentication
+  devise_for :users
+
+  # Pages
+  resources :pages
 end
